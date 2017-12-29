bc3bfdbf5f8bd5b61bdd5ba4ea7a4b4f5cf76f49
Burak Albayrak <balbayrak@hurriyet.com.tr>
2017-12-29 18:45:30 +0300

    NO-ISSUE / WebApi Products ve token Controller lar eklendi.

---
+++ HurriyetDemo/Hurriyet.HurriyetDb.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,113 @@
+﻿using Hurriyet.HurriyetDb.Business.Abstract;
+
+namespace Hurriyet.HurriyetDb.WebApi.Controllers
+{
+    using System;
+    using HurriyetDemo.HurriyetDb.Entities.Concrete;
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace Hurriyet.HurriyetDb.WebApi.Controllers
+    {
+        [Route("api/[controller]")]
+        public class ProductsController : Controller
+        {
+            private IProductService _productService;
+            public ProductsController(IProductService productService)
+            {
+                _productService = productService;
+            }
+
+            [HttpGet]
+            public IActionResult Get()
+            {
+                try
+                {
+                    var products = _productService.GetAll();
+                    if (products == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(products);
+                }
+                catch (Exception e)
+                {
+                    return new StatusCodeResult(500);
+                }
+            }
+
+            [HttpGet("{productId}")]
+            public IActionResult Get(int productId)
+            {
+                try
+                {
+                    var products = _productService.GetById(p => p.Id == productId);
+
+                    if (products == null)
+                    {
+                        return NotFound($"Product not found. Product Id : {productId}");
+                    }
+                    return Ok(products);
+                }
+                catch (Exception e)
+                {
+                    return new StatusCodeResult(500);
+                }
+            }
+
+            [HttpPost]
+            public IActionResult Post([FromBody]Product product)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        _productService.Add(product);
+                        return new StatusCodeResult(201);
+                    }
+                    return BadRequest();
+                }
+                catch (Exception e)
+                {
+                    return new StatusCodeResult(500);
+                }
+            }
+
+            [HttpPut]
+            public IActionResult Put(Product product)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        _productService.Update(product);
+                        return Ok(product);
+                    }
+                    return BadRequest();
+                }
+                catch (Exception e)
+                {
+                    return new StatusCodeResult(500);
+                }
+            }
+
+            [HttpDelete("{productId}")]
+            public IActionResult Delete(int productId)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        _productService.Delete(productId);
+                        return Ok();
+                    }
+                    return BadRequest();
+                }
+                catch (Exception e)
+                {
+                    return new StatusCodeResult(500);
+                }
+            }
+        }
+    }
+
+}
---
+++ HurriyetDemo/Hurriyet.HurriyetDb.WebApi/Controllers/TokenController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hurriyet.HurriyetDb.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
+    using System.Security.Claims;
+    using System.Text;
+    using System.Threading.Tasks;
+    using HurriyetDemo.HurriyetDb.Entities.Concrete;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.IdentityModel.Tokens;
+
+    namespace Hurriyet.HurriyetDb.WebApi.Controllers
+    {
+        [AllowAnonymous]
+        [Route("api/[controller]")]
+        public class TokenController : Controller
+        {
+            [HttpPost("new")]
+            public IActionResult GetToken([FromBody]User user)
+            {
+                //if (IsValidUserAndPassword(user.Username, user.Password))
+
+                if (ModelState.IsValid)
+                {
+                    return new ObjectResult(GenerateToken(user));
+                }
+
+                return Unauthorized();
+            }
+
+            private string GenerateToken(User user)
+            {
+                var someClaims = new Claim[]{
+                    new Claim(JwtRegisteredClaimNames.UniqueName,user.Username),
+                    new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                    new Claim(JwtRegisteredClaimNames.NameId,Guid.NewGuid().ToString())
+                };
+
+                SecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("uzun ince bir yoldayım şarkısını buradan tüm sevdiklerime hediye etmek istiyorum mümkün müdür acaba?"));
+                var token = new JwtSecurityToken(
+                    issuer: "balbayrak.com.tr",
+                    audience: "heimdall.fabrikam.com",
+                    claims: someClaims,
+                    expires: DateTime.Now.AddMinutes(1),
+                    signingCredentials: new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256)
+                );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+
+            private bool IsValidUserAndPassword(string userName, string password)
+            {
+                //Sürekli true dönüyor. Normalde bir Identity mekanizması ile entegre etmemiz lazım.
+                return true;
+            }
+        }
+    }
+
+}
