191e9de00de777dd6a1dfae2422fa3df21af859c
Burak Albayrak <balbayrak@hurriyet.com.tr>
2017-12-29 18:39:26 +0300

    NO-ISSUE / Web Api katmanı eklendi

---
+++ HurriyetDemo/Hurriyet.HurriyetDb.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hurriyet.HurriyetDb.WebApi.Controllers
+{
+    public class LoginController
+    {
+    }
+}
